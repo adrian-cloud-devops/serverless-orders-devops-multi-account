96d6fd1a9185f504ce5bbaa2e15fc2b6076946d0
adrianjozefowski-polsl <56541592+adrianjozefowski-polsl@users.noreply.github.com>
2026-03-29 20:28:01 +0200

    remove aws profile for CI compatibility

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,8 +14,8 @@ terraform {
 }
 
 provider "aws" {
-  alias   = "api"
-  region  = var.aws_region
+  alias  = "api"
+  region = var.aws_region
 
   assume_role {
     role_arn = "arn:aws:iam::${var.account_a_api_id}:role/${var.terraform_deploy_role_name}"
@@ -23,9 +23,9 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias   = "data"
-  region  = var.aws_region
-  
+  alias  = "data"
+  region = var.aws_region
+
   assume_role {
     role_arn = "arn:aws:iam::${var.account_b_data_id}:role/${var.terraform_deploy_role_name}"
   }
